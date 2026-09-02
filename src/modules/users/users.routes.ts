3ca4f9e5af34b68018d1c51188b489391714798f
Evo <casagrandeevo@gmail.com>
2026-09-02 14:54:56 -0300

    Se levanto el servidor y se estructuró el proyecto

---
+++ src/modules/users/users.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.
+    route('/').
+    get().
+    post()
+
+router.
+    route('/').
+    get().
+    patch().
+    delete()
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import app from './app';
+import * as dotenv from 'dotenv';
+
+dotenv.config( {path: '../.env'});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('El servidor esta escuchando correctamente!')
+})
